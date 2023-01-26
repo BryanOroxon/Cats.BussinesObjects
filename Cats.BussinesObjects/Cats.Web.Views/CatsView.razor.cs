44f75ba0684a145cd61ace44f0a83766258e2a91
RicardoJosuePerezAltamirano <ricardoj.altamirano@outlook.com>
2023-01-26 09:37:27 -0600

    incorporacion de cliente blazor con consumo de gatos

---
+++ Cats.BussinesObjects/Cats.Web.Views/CatsView.razor.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+namespace Cats.Web.Views
+{
+    public partial class CatsView : ICatsView
+    {
+        [Inject]
+        public ICatsViewModels viewModel { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await RenderCatsAsync();
+        }
+        public async Task RenderCatsAsync()
+        {
+            await viewModel.GetCatsAsync();
+            StateHasChanged();
+        }
+    }
+}
